d22f3fe1d066811bcd94df09cc2852ae85a2158b
Eve <evelynluu2@gmail.com>
2017-08-10 18:54:15 -0700

    Took out contact form fade

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,5 @@
 var submitted=false;
 
 $('#gform').on('submit', function(e) {
-  $('#gform *').fadeOut(2000);
-  $('#gform').prepend('Thank you for your message! I will reply back very soon.');
-
-  setTimeout(function(){
-    location.reload();
-  }, 30000);
+  $('#gform').prepend('<p>Thank you for your message! I will reply soon.</p>');
 });
