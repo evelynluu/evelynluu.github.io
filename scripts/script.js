968a3cd23bc8d483e2d8c055a445b16f6bf7b0f8
Eve <evelynluu2@gmail.com>
2017-08-10 19:00:37 -0700

    Editing contact submission message

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@
 var submitted=false;
 
 $('#gform').on('submit', function(e) {
-  $('#gform').prepend('<p>Thank you for your message! I will reply soon.</p>');
+  $('#gform').prepend('Thank you for your message! I will reply soon.');
 });
