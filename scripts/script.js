601e358b776c2fc61474b0f72429c04a532888c4
Eve <evelynluu2@gmail.com>
2017-08-11 10:49:45 -0700

    Reorderd some things in contact

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,4 @@
 
-var submitted=false;
-
 $('#gform').on('submit', function(e) {
   $('#gform').prepend('<p class="submission-msg">Thank you for your message! I will reply soon.</p>');
-  this.reset();
 });
