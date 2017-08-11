7feed8f80a27d3e824606460f77003ef537b2505
Eve <evelynluu2@gmail.com>
2017-08-10 19:07:06 -0700

    Testing

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@
 var submitted=false;
 
 $('#gform').on('submit', function(e) {
-  $('#gform').prepend('<p class="submission-msg">Thank you for your message! I will reply soon.</p>');
+  $('#gform').prepend('<p class="submission-msg">This is a test.</p>');
 });
